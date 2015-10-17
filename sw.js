2d60345d9692b80095754ce4d8c2796529f53a59
Sajib Devnath <sajibdevnathakash@gmail.com>
2015-10-17 16:42:34 +0600

    [update]

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+importScripts('serviceworker-cache-polyfill.js');
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function(registration) {
     // Registration was successful
