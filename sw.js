105cf82013c14966636c4ad48d646fadd44e4907
Sajib Devnath Akash <sajibdevnathakash@gmail.com>
2015-10-20 04:01:29 +0600

    rebuild sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 // Register
-
 if ('serviceWorker' in navigator ) {
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker.register('/sw.js')
         .then(function (reg) {
             // success
             console.log('Registration succeded. Scope is ' + reg.scope);
@@ -11,3 +10,21 @@ if ('serviceWorker' in navigator ) {
             console.log('Registration failed with ' + err);
         })
 }
+
+// install and activate
+this.addEventListener('install', function (event) {
+    // Service Worker will not install until the 
+    // code inside waitUntil() successfully occur
+    event.waitUntil(
+        // Caches.open() method to create a new cache called v1
+        caches.open('v1')
+            .then(function (cache) {
+                return cache.addAll([
+                    '/index.html',
+                    '/serviceworker-cache-polyfill.js',
+                    '/style.css',
+                    '/sw.js'
+                )];
+            });    
+        ); 
+});
