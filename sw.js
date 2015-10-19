7fa1137ea7920d8408e1c7f077808e7706d985d1
Sajib Devnath Akash <sajibdevnathakash@gmail.com>
2015-10-20 04:28:08 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,8 +17,7 @@ this.addEventListener('install', function (event) {
      // code inside waitUntil() successfully occur
      event.waitUntil(
 -        // Caches.open() method to create a new cache called v1
-        caches.open('v1')
-            .then(function (cache) {
+        caches.open('v1').then(function (cache) {
                 return cache.addAll([
                     '/index.html',
                     '/serviceworker-cache-polyfill.js',
