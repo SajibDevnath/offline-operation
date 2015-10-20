63157b5390d4bc2fe2cbd4f0b67517c0c7864b10
Sajib Devnath Akash <sajibdevnathakash@gmail.com>
2015-10-20 12:16:21 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -32,6 +32,13 @@ this.addEventListener('install', function (event) {
 // returning the cache
 this.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
+    caches.match(event.request).catch(function() {
+      return fetch(event.request).then(function(response) {
+        return caches.open('v1').then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });  
+      });
+    })
   );
 });
